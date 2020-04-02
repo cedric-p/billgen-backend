b809f0ac4717cf735982eb953af3051aa65f0921
Cédric PAUCHET <cedric.pauchet@ext.leroymerlin.fr>
2020-04-02 18:53:59 +0200

    feat(spring): Create spring base project with spring initializr

---
+++ src/main/java/com/cedricp/billgen/BillGenApplication.java
@@ -0,0 +1,13 @@
+package com.cedricp.billgen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillGenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillGenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cedricp/billgen/BillGenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cedricp.billgen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillGenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
